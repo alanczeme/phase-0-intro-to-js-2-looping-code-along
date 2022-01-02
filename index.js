18976c589cb8a2710903c50fd5957d57b8e4e306
Alan Czemerinski <alanczeme@gmail.com>
2022-01-01 21:00:42 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(arr, event) {
+    const allMessages = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        allMessages.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return allMessages;
+}
+
+function countDown(int) {
+    while(int >= 0) {
+        console.log(int);
+        int--;
+    }
+}
